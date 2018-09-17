889211e82977f7109e590372075af9a4c08dad8f
Calvin Hu <ycalvinhu@gmail.com>
2018-09-17 15:55:04 -0700

    Added carousel to projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import classes from './Projects.css';
+import shmack1 from '../../assets/images/shmack1.png';
 
 const projects = props => {
-  let shmackClasses = classes.Project + ' ' + classes.Hide;
+  let shmackClasses = classes.ProjectItem + ' ' + classes.Hide;
   if (props.isShowShmack)
-    shmackClasses = classes.Project + ' ' + classes.BlockSlideFadeIn;
+    shmackClasses = classes.ProjectItem + ' ' + classes.BlockSlideFadeIn;
 
   let projectsClasses = classes.Projects + ' ' + classes.Hide;
   if (props.isShowProjects)
@@ -14,7 +15,26 @@ const projects = props => {
   return (
     <div className={projectsClasses}>
       <h4>My Projects</h4>
-      <div className={shmackClasses} />
+      <div className={shmackClasses}>
+        <div className={classes.Carousel}>
+          <div className={classes.CarouselItem}>
+            <div className={classes.ImgContainer}>
+              <img src={shmack1} alt="shmack" />
+            </div>
+            <div className={classes.Description}>
+              <p>Get popular food items fast</p>
+            </div>
+          </div>
+          <div className={classes.CarouselItem}>
+            <div className={classes.ImgContainer}>
+              <img src={shmack1} alt="shmack" />
+            </div>
+            <div className={classes.Description}>
+              <p>Get popular food items fast</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
