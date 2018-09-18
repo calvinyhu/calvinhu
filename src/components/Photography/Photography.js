397b2c04b7198ca361b69742a20e8dbef08523d7
Calvin Hu <ycalvinhu@gmail.com>
2018-09-17 20:16:24 -0700

    Added gallery

--- src/components/Photography/Photography.js
+++ src/components/Photography/Photography.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import classes from './Photography.css';
+import gallery from '../../assets/images/gallery.jpg';
+import myphoto1 from '../../assets/images/myphoto1.png';
 
 const photography = props => {
   let photographyClasses = classes.Photography + ' ' + classes.Hide;
@@ -10,7 +12,14 @@ const photography = props => {
   return (
     <div className={photographyClasses}>
       <h4>My Photos</h4>
-      <div className={classes.Gallery}>Gallery</div>
+      <div className={classes.Gallery}>
+        <div className={classes.GalleryBackground}>
+          <img src={gallery} alt="gallery" />
+          <div className={classes.GalleryItem}>
+            <img src={myphoto1} alt="my" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
