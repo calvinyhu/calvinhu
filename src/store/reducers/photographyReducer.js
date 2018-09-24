22e71bcf0bd4ff9cb674a650f4886a53c8ce3f8f
Calvin Hu <ycalvinhu@gmail.com>
2018-09-24 13:47:50 -0700

    Fixed photography load bug

--- src/store/reducers/photographyReducer.js
+++ src/store/reducers/photographyReducer.js
@@ -2,7 +2,7 @@ import * as actionTypes from '../actions/actionTypes';
 import { updateObject } from '../../utils/utilities';
 
 const initialState = {
-  photos: {}
+  photos: null
 };
 
 const setPhotos = (state, action) => {
