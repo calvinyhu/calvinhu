ff19b808bf4df22b696d7d7b3b96dcc76324393f
Calvin Hu <ycalvinhu@gmail.com>
2018-09-26 20:07:07 -0700

    Added health targets

--- src/containers/Fitness/Fitness.js
+++ src/containers/Fitness/Fitness.js
@@ -92,98 +92,106 @@ class Fitness extends PureComponent {
 
     const nutrition = (
       <div className={classes.NutritionContainer}>
-        <h3>Targets</h3>
-        <Reveal effect={classes.BlockSlideFadeIn}>
-          <div className={classes.Nutrition}>
-            <div className={classes.Macros}>
-              <p>Daily Macros (% grams)</p>
-              <canvas id={macroPieId} />
+        <h3>My Health Targets</h3>
+        <div className={classes.Nutrition}>
+          <Reveal effect={classes.BlockSlideFadeIn}>
+            <div className={classes.Target}>
+              <p>Weigh</p>
+              <h3>160</h3>
+              <p>pounds (currently 152)</p>
             </div>
-          </div>
-        </Reveal>
+            <div className={classes.Target}>
+              <p>Consume</p>
+              <h3>3200</h3>
+              <p>calories daily</p>
+            </div>
+            <div className={classes.Target}>
+              <p>Distribute macros daily</p>
+              <canvas id={macroPieId} />
+              <p>% grams</p>
+            </div>
+          </Reveal>
+        </div>
       </div>
     );
 
     const diet = (
       <div className={classes.DietContainer}>
-        <Reveal effect={classes.BlockSlideFadeIn}>
-          <h3>Example Diet</h3>
-          <div className={classes.Diet}>
-            <Reveal effect={classes.BlockSlideFadeIn}>
-              <div className={classes.Meal}>
-                <h5>Breakfast</h5>
-                <p>3 Eggs</p>
-                <p>Mixed Veggies</p>
-              </div>
-              <div className={classes.Meal}>
-                <h5>Lunch</h5>
-                <p>Pasta</p>
-              </div>
-              <div className={classes.Meal}>
-                <h5>Post Workout</h5>
-                <p>Greek Yogurt Fruit Smoothie</p>
-              </div>
-              <div className={classes.Meal}>
-                <h5>Dinner</h5>
-                <p>Chicken Breast</p>
-                <p>Broccolli</p>
-                <p>Brown Rice</p>
-              </div>
-              <div className={classes.Meal}>
-                <h5>Late Night</h5>
-                <p>Peanut Butter Banana Bread Toast</p>
-              </div>
-            </Reveal>
-          </div>
-        </Reveal>
+        <h3>My Usual Diet</h3>
+        <div className={classes.Diet}>
+          <Reveal effect={classes.BlockSlideFadeIn}>
+            <div className={classes.Meal}>
+              <h5>Breakfast</h5>
+              <p>3 Eggs</p>
+              <p>Deli Meat</p>
+              <p>Mixed Vegetables</p>
+            </div>
+            <div className={classes.Meal}>
+              <h5>Lunch</h5>
+              <p>Varies (carb heavy)</p>
+            </div>
+            <div className={classes.Meal}>
+              <h5>Post Workout</h5>
+              <p>Greek Yogurt Fruit Smoothie</p>
+            </div>
+            <div className={classes.Meal}>
+              <h5>Dinner</h5>
+              <p>Varies (protein heavy)</p>
+            </div>
+            <div className={classes.Meal}>
+              <h5>Late Night</h5>
+              <p>Peanut Butter Banana Bread Toast</p>
+            </div>
+          </Reveal>
+        </div>
       </div>
     );
 
     const workout = (
       <div className={classes.WorkoutContainer}>
-        <Reveal effect={classes.BlockSlideFadeIn}>
-          <h3>Example Week</h3>
-          <div className={classes.Workout}>
-            <Reveal effect={classes.BlockSlideFadeIn}>
-              <div className={classes.Day}>
-                <h5>Monday</h5>
-                <p>Pseudo Planche Push Ups</p>
-                <p>Ring Dips</p>
-                <p>Dumbbell Squats</p>
-                <p>Calf Press</p>
-              </div>
-              <div className={classes.Day}>
-                <h5>Tuesday</h5>
-                <p>Front Lever Holds</p>
-                <p>Pull Ups</p>
-                <p>Deadlifts</p>
-                <p>V-Ups</p>
-              </div>
-              <div className={classes.Day}>
-                <h5>Thursday</h5>
-                <p>One Handed Push Ups</p>
-                <p>Weighted Dips</p>
-                <p>Barbell Squats</p>
-                <p>Calf Press</p>
-              </div>
-              <div className={classes.Day}>
-                <h5>Friday</h5>
-                <p>One Handed Chin Up Hold</p>
-                <p>Chin Ups</p>
-                <p>Reverse Deadlifts</p>
-                <p>V-Ups</p>
-              </div>
-            </Reveal>
-          </div>
-        </Reveal>
+        <h3>My Usual Routine</h3>
+        <div className={classes.Workout}>
+          <Reveal effect={classes.BlockSlideFadeIn}>
+            <div className={classes.Day}>
+              <h5>Monday</h5>
+              <p>Pseudo Planche Push Ups</p>
+              <p>Ring Dips</p>
+              <p>Dumbbell Squats</p>
+              <p>Calf Press</p>
+            </div>
+            <div className={classes.Day}>
+              <h5>Tuesday</h5>
+              <p>Front Lever Holds</p>
+              <p>Pull Ups</p>
+              <p>Deadlifts</p>
+              <p>V-Ups</p>
+            </div>
+            <div className={classes.Day}>
+              <h5>Thursday</h5>
+              <p>One Handed Push Ups</p>
+              <p>Weighted Dips</p>
+              <p>Barbell Squats</p>
+              <p>Calf Press</p>
+            </div>
+            <div className={classes.Day}>
+              <h5>Friday</h5>
+              <p>One Handed Chin Up Hold</p>
+              <p>Chin Ups</p>
+              <p>Reverse Deadlifts</p>
+              <p>V-Ups</p>
+            </div>
+          </Reveal>
+        </div>
       </div>
     );
 
     const current = (
       <div className={classes.Current}>
-        {nutrition}
-        {diet}
-        {workout}
+        <Reveal effect={classes.BlockSlideFadeIn}>
+          {nutrition}
+          {diet}
+          {workout}
+        </Reveal>
       </div>
     );
 
