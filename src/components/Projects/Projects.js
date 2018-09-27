2180dee9ae37e2e7124ae84a9a45b13aafb9f0d3
Calvin Hu <ycalvinhu@gmail.com>
2018-09-27 12:49:45 -0700

    Changed styling

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import Reveal from 'react-reveal/Reveal';
 
 import classes from './Projects.css';
 import shmack1 from '../../assets/images/shmack1.png';
+import shmack2 from '../../assets/images/shmack2.png';
 
 class Projects extends PureComponent {
   componentDidMount() {
@@ -14,26 +15,39 @@ class Projects extends PureComponent {
       <div className={classes.ProjectItem}>
         <div className={classes.ColorSplash} />
         <div className={classes.ProjectContent}>
-          <p>
-            Shmack is a platform where users can view, add, and vote on popular
-            menu items at restaurants. This app aims to be fast, simple, and
-            intuitive for finding quick eats.
-          </p>
+          <div>
+            <p>
+              <a
+                className={classes.ImgContainer}
+                href="https://shmack.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Shmack
+              </a>{' '}
+              is a platform where users can view, add, and vote on popular menu
+              items at restaurants.
+            </p>
+            <p>
+              This app aims to be fast, simple, and intuitive for finding quick
+              eats.
+            </p>
+          </div>
           <div className={classes.Carousel}>
             <div className={classes.CarouselItem}>
               <div className={classes.ImgContainer}>
-                <img src={shmack1} alt="shmack" />
+                <img src={shmack1} alt="shmack1" />
               </div>
               <div className={classes.Description}>
-                <p>Get popular food items fast</p>
+                <p>Know what's popular fast</p>
               </div>
             </div>
             <div className={classes.CarouselItem}>
               <div className={classes.ImgContainer}>
-                <img src={shmack1} alt="shmack" />
+                <img src={shmack2} alt="shmack2" />
               </div>
               <div className={classes.Description}>
-                <p>Get popular food items fast</p>
+                <p>Search around</p>
               </div>
             </div>
           </div>
@@ -44,7 +58,7 @@ class Projects extends PureComponent {
     return (
       <Reveal effect={classes.BlockSlideFadeIn}>
         <div className={classes.Projects}>
-          <h3>Web Apps I've Built</h3>
+          <h3>Web Apps I'm Building</h3>
           {shmack}
         </div>
       </Reveal>
