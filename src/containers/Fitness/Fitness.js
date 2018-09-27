ec121534f56225bf7076d5165fcd3570041b68e0
Calvin Hu <ycalvinhu@gmail.com>
2018-09-26 17:21:29 -0700

    Added charts.js

--- src/containers/Fitness/Fitness.js
+++ src/containers/Fitness/Fitness.js
@@ -2,10 +2,13 @@ import React, { PureComponent } from 'react';
 import { NavLink } from 'react-router-dom';
 import Reveal from 'react-reveal/Reveal';
 import throttle from 'raf-throttle';
+import Chart from 'chart.js';
 
 import classes from './Fitness.css';
 import STYLES from '../../utils/styles';
 import Button from '../../components/UI/Button/Button';
+
+const macroPieId = 'macroPieId';
 
 class Fitness extends PureComponent {
   constructor(props) {
@@ -14,8 +17,14 @@ class Fitness extends PureComponent {
   }
 
   state = {
-    isShowBackToTopButton: false
+    isShowBackToTopButton: false,
+    macroPie: null
   };
+
+  isCreated = false;
+  componentDidMount() {
+    this.setState({ macroPie: document.getElementById(macroPieId) });
+  }
 
   handleScroll = event => {
     throttle(
@@ -64,79 +73,118 @@ class Fitness extends PureComponent {
       </div>
     );
 
+    if (this.state.macroPie && !this.isCreated) {
+      new Chart(this.state.macroPie, {
+        type: 'doughnut',
+        data: {
+          labels: ['Protein', 'Carbs', 'Fat'],
+          datasets: [
+            {
+              data: [20, 50, 30],
+              backgroundColor: ['#ffa07a', '#54af88', '#5481a4']
+            }
+          ]
+        }
+      });
+      this.isCreated = true;
+    }
+
+    const nutrition = (
+      <div className={classes.NutritionContainer}>
+        <h3>Targets</h3>
+        <div className={classes.Nutrition}>
+          <div className={classes.Macros}>
+            <p>Daily Macros (% grams)</p>
+            <canvas id={macroPieId} />
+          </div>
+        </div>
+      </div>
+    );
+
+    const diet = (
+      <div className={classes.DietContainer}>
+        <h3>Example Diet</h3>
+        <div className={classes.Diet}>
+          <div className={classes.Meal}>
+            <h4>Breakfast</h4>
+            <p>3 Eggs</p>
+            <p>Mixed Veggies</p>
+          </div>
+          <div className={classes.Meal}>
+            <h4>Lunch</h4>
+            <p>Pasta</p>
+          </div>
+          <div className={classes.Meal}>
+            <h4>Post Workout</h4>
+            <p>Greek Yogurt Fruit Smoothie</p>
+          </div>
+          <div className={classes.Meal}>
+            <h4>Dinner</h4>
+            <p>Chicken Breast</p>
+            <p>Broccolli</p>
+            <p>Brown Rice</p>
+          </div>
+          <div className={classes.Meal}>
+            <h4>Late Night</h4>
+            <p>Peanut Butter Banana Bread Toast</p>
+          </div>
+        </div>
+      </div>
+    );
+
+    const workout = (
+      <div className={classes.WorkoutContainer}>
+        <h3>Example Week</h3>
+        <div className={classes.Workout}>
+          <div className={classes.Day}>
+            <h4>Day 1</h4>
+            <p>Pseudo Planche Push Ups</p>
+            <p>Ring Dips</p>
+            <p>Dumbbell Squats</p>
+            <p>Calf Press</p>
+          </div>
+          <div className={classes.Day}>
+            <h4>Day 2</h4>
+            <p>Front Lever Holds</p>
+            <p>Pull Ups</p>
+            <p>Deadlifts</p>
+            <p>V-Ups</p>
+          </div>
+          <div className={classes.Day}>
+            <h4>Day 3</h4>
+            <p>Recover</p>
+          </div>
+          <div className={classes.Day}>
+            <h4>Day 4</h4>
+            <p>One Handed Push Ups</p>
+            <p>Weighted Dips</p>
+            <p>Barbell Squats</p>
+            <p>Calf Press</p>
+          </div>
+          <div className={classes.Day}>
+            <h4>Day 5</h4>
+            <p>One Handed Chin Up Hold</p>
+            <p>Chin Ups</p>
+            <p>Reverse Deadlifts</p>
+            <p>V-Ups</p>
+          </div>
+          <div className={classes.Day}>
+            <h4>Day 6</h4>
+            <p>Recover</p>
+          </div>
+          <div className={classes.Day}>
+            <h4>Day 7</h4>
+            <p>Recover</p>
+          </div>
+        </div>
+      </div>
+    );
+
     const current = (
       <div className={classes.Current}>
-        <div className={classes.DietContainer}>
-          <h3>My Diet</h3>
-          <div className={classes.Diet}>
-            <div className={classes.Meal}>
-              <h4>Breakfast</h4>
-              <p>3 Eggs</p>
-              <p>Mixed Veggies</p>
-            </div>
-            <div className={classes.Meal}>
-              <h4>Lunch</h4>
-              <p>Carb Heavy</p>
-            </div>
-            <div className={classes.Meal}>
-              <h4>Post Workout</h4>
-              <p>Greek Yogurt Fruit Smoothie</p>
-            </div>
-            <div className={classes.Meal}>
-              <h4>Dinner</h4>
-              <p>Protein and Rice</p>
-            </div>
-            <div className={classes.Meal}>
-              <h4>Late Night</h4>
-              <p>Peanut Butter Banana Bread Toast</p>
-            </div>
-          </div>
-        </div>
-        <div className={classes.WorkoutContainer}>
-          <h3>My Week</h3>
-          <div className={classes.Workout}>
-            <div className={classes.Day}>
-              <h4>Day 1</h4>
-              <p>Pseudo Planche Push Ups</p>
-              <p>Ring Dips</p>
-              <p>Dumbbell Squats</p>
-              <p>Calf Press</p>
-            </div>
-            <div className={classes.Day}>
-              <h4>Day 2</h4>
-              <p>Front Lever Holds</p>
-              <p>Pull Ups</p>
-              <p>Deadlifts</p>
-              <p>V-Ups</p>
-            </div>
-            <div className={classes.Day}>
-              <h4>Day 3</h4>
-              <p>Recover</p>
-            </div>
-            <div className={classes.Day}>
-              <h4>Day 4</h4>
-              <p>One Handed Push Ups</p>
-              <p>Weighted Dips</p>
-              <p>Barbell Squats</p>
-              <p>Calf Press</p>
-            </div>
-            <div className={classes.Day}>
-              <h4>Day 5</h4>
-              <p>One Handed Chin Up Hold</p>
-              <p>Chin Ups</p>
-              <p>Reverse Deadlifts</p>
-              <p>V-Ups</p>
-            </div>
-            <div className={classes.Day}>
-              <h4>Day 6</h4>
-              <p>Recover</p>
-            </div>
-            <div className={classes.Day}>
-              <h4>Day 7</h4>
-              <p>Recover</p>
-            </div>
-          </div>
-        </div>
+        {nutrition}
+        {diet}
+        {workout}
       </div>
     );
 
@@ -144,8 +192,9 @@ class Fitness extends PureComponent {
       <div className={classes.YearSection}>
         <h4>2013</h4>
         <div className={classes.YearSummary}>
-          <p>Started body building routine</p>
-          <p>6 day routine</p>
+          <p>Inspired by Jesse Wellens and Jeff Cavaliere</p>
+          <p>Started 6 day bodybuilding home workout</p>
+          <p>Starting weight was 135</p>
         </div>
       </div>
     );
@@ -153,64 +202,20 @@ class Fitness extends PureComponent {
       <div className={classes.YearSection}>
         <h4>2014</h4>
         <div className={classes.YearSummary}>
-          <p>Mid-year switched to calisthenics training</p>
-          <p>Inspired by Frank Medrano</p>
-        </div>
-      </div>
-    );
-    const year2015 = (
-      <div className={classes.YearSection}>
-        <h4>2015</h4>
-        <div className={classes.YearSummary}>
-          <p>End of year weight plateau at 153 pounds</p>
-        </div>
-      </div>
-    );
-    const year2016 = (
-      <div className={classes.YearSection}>
-        <h4>2016</h4>
-        <div className={classes.YearSummary}>
-          <p>Lost motivation beginning of 2016</p>
-        </div>
-      </div>
-    );
-    const year2017 = (
-      <div className={classes.YearSection}>
-        <h4>2017</h4>
-        <div className={classes.YearSummary}>
-          <p>Maintaining body weight</p>
-        </div>
-      </div>
-    );
-    const year2018 = (
-      <div className={classes.YearSection}>
-        <h4>2018</h4>
-        <div className={classes.YearSummary}>
-          <p>Motivation is back starting 2018</p>
-        </div>
-      </div>
-    );
-    const year2019 = (
-      <div className={classes.YearSection}>
-        <h4>2019</h4>
-        <div className={classes.YearSummary}>
-          <p>?</p>
+          <p>Inspired by Frank Medrano and Dominik Sky</p>
+          <p>Changed to 4 day calisthenics routine</p>
+          <p>Starting weight was 145</p>
         </div>
       </div>
     );
 
     const timeline = (
       <div className={classes.TimelineContainer}>
-        <h3>How did I get to where I am?</h3>
+        <h3>My Beginning</h3>
         <div className={classes.Timeline}>
           <Reveal effect={classes.BlockSlideFadeIn}>
             {year2013}
             {year2014}
-            {year2015}
-            {year2016}
-            {year2017}
-            {year2018}
-            {year2019}
           </Reveal>
         </div>
       </div>
