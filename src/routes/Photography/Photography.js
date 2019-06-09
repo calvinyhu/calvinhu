ef3c1e48c872249149fe30b368b1750fe91dd684
Calvin Hu <ycalvinhu@gmail.com>
2019-06-08 21:12:34 -0700

    Refactor Photography

--- src/routes/Photography/Photography.js
+++ src/routes/Photography/Photography.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Fade from 'react-reveal/Fade';
 import throttle from 'raf-throttle';
 import classnames from 'classnames';
@@ -8,123 +8,110 @@ import Gallery from '../../components/Gallery/Gallery';
 import Fa from '../../components/UI/Icon/Fa/Fa';
 import { firestore, storage } from '../../firebase';
 
-let photos = null;
-let totalNumPhotos = 0;
+let initialPhotos = null;
+let initialTotalNumPhotos = 0;
 let timeout = null;
 
-class Photography extends React.PureComponent {
-  isAlive = false;
+const Photography = () => {
+  const [photos, setPhotos] = useState(initialPhotos);
+  const [totalNumPhotos, setTotalNumPhotos] = useState(initialTotalNumPhotos);
+  useEffect(() => {
+    const getUrls = data => {
+      const urlPromises = [];
 
-  state = {
-    isHideTouchApp: false,
-    numPhotos: 10,
-    photos: photos,
-    totalNumPhotos: totalNumPhotos,
-  };
-
-  componentDidMount() {
-    this.isAlive = true;
-    window.addEventListener('scroll', this.handleScroll);
-    timeout = setTimeout(() => this.setState({ isHideTouchApp: true }), 7000);
-
-    if (window.top !== 0) window.scrollTo({ top: 0 });
-    if (!this.state.photos) this.getPhotos();
-  }
-
-  componentWillUnmount() {
-    this.isAlive = false;
-    window.removeEventListener('scroll', this.handleScroll);
-    clearTimeout(timeout);
-  }
-
-  handleScroll = () => throttle(this.showMorePhotos());
-
-  showMorePhotos = () => {
-    const scrollHeight = Math.max(
-      document.body.scrollHeight,
-      document.documentElement.scrollHeight,
-      document.body.offsetHeight,
-      document.documentElement.offsetHeight,
-      document.body.clientHeight,
-      document.documentElement.clientHeight,
-    );
-
-    const isAtBottom = window.scrollY > scrollHeight - window.innerHeight * 1.3;
-    const isMorePhotos = this.state.numPhotos < this.state.totalNumPhotos;
-
-    if (isAtBottom && isMorePhotos) {
-      this.setState(prevState => {
-        return { numPhotos: prevState.numPhotos + 10 };
+      const ids = Object.keys(data);
+      ids.forEach(id => {
+        const urlPromise = storage.ref(`photography/${id}`).getDownloadURL();
+        urlPromises.push(urlPromise);
       });
-    }
-  };
 
-  getPhotos = async () => {
-    let data = null;
-    const urls = await firestore
-      .collection('photography')
-      .doc('photoDetails')
-      .get()
-      .then(doc => {
-        if (doc.exists) {
-          data = doc.data();
-          return this.getUrls(data);
-        } else console.log('Photo details do not exist!');
-      })
-      .catch(error => console.log(error));
+      return Promise.all(urlPromises).then(urls => urls);
+    };
 
-    const ids = Object.keys(data);
-    ids.forEach((id, index) => (data[id].url = urls[index]));
+    const getPhotos = async () => {
+      let data = null;
+      const urls = await firestore
+        .collection('photography')
+        .doc('photoDetails')
+        .get()
+        .then(doc => {
+          if (doc.exists) {
+            data = doc.data();
+            return getUrls(data);
+          } else console.log('Photo details do not exist!');
+        })
+        .catch(error => console.log(error));
 
-    photos = data;
-    totalNumPhotos = ids.length;
+      const ids = Object.keys(data);
+      ids.forEach((id, index) => (data[id].url = urls[index]));
 
-    if (!this.isAlive) return;
-    this.setState({ photos: data, totalNumPhotos: ids.length });
-  };
+      initialPhotos = data;
+      initialTotalNumPhotos = ids.length;
 
-  getUrls = data => {
-    const urlPromises = [];
+      setPhotos(data);
+      setTotalNumPhotos(ids.length);
+    };
 
-    const ids = Object.keys(data);
-    ids.forEach(id => {
-      const urlPromise = storage.ref(`photography/${id}`).getDownloadURL();
-      urlPromises.push(urlPromise);
-    });
+    if (!photos) getPhotos();
+  });
 
-    return Promise.all(urlPromises).then(urls => urls);
-  };
+  const [numPhotos, setNumPhotos] = useState(10);
+  useEffect(() => {
+    const showMorePhotos = () => {
+      const scrollHeight = Math.max(
+        document.body.scrollHeight,
+        document.documentElement.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.offsetHeight,
+        document.body.clientHeight,
+        document.documentElement.clientHeight,
+      );
+      const isAtBottom =
+        window.scrollY > scrollHeight - window.innerHeight * 1.3;
+      const isMorePhotos = numPhotos < totalNumPhotos;
 
-  render() {
-    let loader = null;
-    if (!this.state.photos) {
-      loader = (
+      if (isAtBottom && isMorePhotos) setNumPhotos(numPhotos + 10);
+    };
+
+    const handleScroll = () => throttle(showMorePhotos());
+
+    const event = 'scroll';
+    window.addEventListener(event, handleScroll);
+
+    return () => {
+      window.removeEventListener(event, handleScroll);
+    };
+  });
+
+  const [isHideTouchApp, setIsHideTouchApp] = useState(false);
+  useEffect(() => {
+    timeout = setTimeout(() => setIsHideTouchApp(true), 7000);
+    return () => clearTimeout(timeout);
+  });
+
+  const touchAppClasses = classnames({
+    [styles.TouchApp]: true,
+    [styles.HideTouchApp]: isHideTouchApp,
+  });
+  const touchAppIcon = (
+    <div className={touchAppClasses}>
+      <Fade>
+        <Fa lg>fas fa-hand-point-up</Fa>
+      </Fade>
+    </div>
+  );
+
+  return (
+    <div className={styles.PhotographyContainer}>
+      <Gallery numPhotos={numPhotos} photos={photos} />
+      {photos ? null : (
         <div className={styles.LoaderContainer}>
           <div className={styles.Loader} />
         </div>
-      );
-    }
-
-    const touchAppClasses = classnames({
-      [styles.TouchApp]: true,
-      [styles.HideTouchApp]: this.state.isHideTouchApp,
-    });
-    const touchAppIcon = (
-      <div className={touchAppClasses}>
-        <Fade>
-          <Fa lg>fas fa-hand-point-up</Fa>
-        </Fade>
-      </div>
-    );
-
-    return (
-      <div className={styles.PhotographyContainer}>
-        <Gallery numPhotos={this.state.numPhotos} photos={this.state.photos} />
-        {loader}
-        {touchAppIcon}
-      </div>
-    );
-  }
-}
+      )}
+      {touchAppIcon}
+    </div>
+  );
+};
 
 export default Photography;
