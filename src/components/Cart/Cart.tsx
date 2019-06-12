4bfa456c8eab62bfe49d883cd265a4d68ca07285
Calvin Hu <ycalvinhu@gmail.com>
2019-06-11 22:12:28 -0700

    Convert Cart to tsx

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+// @ts-ignore
 import { useDispatch, useSelector } from 'react-redux';
 
 import CartItem from '../CartItem/CartItem';
 import { removeFromCart } from '../../store/actions/cartActions';
 
-const removeFromCartHandlers = {};
+const removeFromCartHandlers: any = {};
 
 const Cart = () => {
+  // @ts-ignore
   const items = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
-  const getRemoveFromCartHandler = id => {
+  const getRemoveFromCartHandler = (id: string) => {
     if (removeFromCartHandlers[id]) return removeFromCartHandlers[id];
     removeFromCartHandlers[id] = () => dispatch(removeFromCart(id));
     return removeFromCartHandlers[id];
