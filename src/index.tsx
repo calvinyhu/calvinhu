a24c1bae938e1d07da55705c5656c3f6e1387cc0
Calvin <calvinhu9@gmail.com>
2020-06-12 19:27:28 -0700

    add antd

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './store/reducers';
 
+import 'antd/dist/antd.css';
 import './index.module.scss';
 
 const composeEnhancers =
