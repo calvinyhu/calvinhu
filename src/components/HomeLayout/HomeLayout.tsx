b05925a136e2629e0466c0928286ae2064680b7d
Calvin <calvinhu9@gmail.com>
2020-06-13 17:16:06 -0700

    fix styling

--- src/components/HomeLayout/HomeLayout.tsx
+++ src/components/HomeLayout/HomeLayout.tsx
@@ -6,6 +6,83 @@ import styles from './HomeLayout.module.scss';
 import { useScrollPositionFlag } from 'utils/hooks';
 import Fa from 'components/UI/Fa/Fa';
 import Button from 'components/UI/Button/Button';
+import NavDrawer from '../NavDrawer/NavDrawer';
+
+interface LinksProps {
+  onClick?: () => void;
+}
+
+const Links: FC<LinksProps> = ({ onClick }) => (
+  <>
+    <Link to="photo" className={styles.NavBarLink} onClick={onClick}>
+      Photography
+    </Link>
+    <Link to="about" className={styles.NavBarLink} onClick={onClick}>
+      About
+    </Link>
+  </>
+);
+
+interface NavBarProps {
+  isShowToTop: boolean;
+  pathname: string;
+  handleDrawerOpen: () => void;
+}
+
+const NavBar: FC<NavBarProps> = ({
+  isShowToTop,
+  pathname,
+  handleDrawerOpen,
+}) => {
+  return (
+    <div
+      className={clsx(styles.NavBar, {
+        [styles.White]: isShowToTop || pathname !== '/',
+      })}
+    >
+      <Link
+        to="."
+        className={clsx(styles.NavBarLink, styles.NavBarHomeLink, {
+          [styles.Clickable]: isShowToTop || pathname !== '/',
+        })}
+      >
+        Calvin Hu
+      </Link>
+      <span className={styles.NavBarRight}>
+        <span className={styles.NavBarLinks}>
+          <Links />
+        </span>
+        <span className={styles.NavBarDrawerToggle}>
+          <Button circle clear click={handleDrawerOpen} ariaLabel="Menu">
+            <Fa>fas fa-stream</Fa>
+          </Button>
+        </span>
+      </span>
+    </div>
+  );
+};
+
+interface TopOfPageButtonProps {
+  isShowToTop: boolean;
+  handleScrollToTop: () => void;
+}
+
+const TopOfPageButton: FC<TopOfPageButtonProps> = ({
+  isShowToTop,
+  handleScrollToTop,
+}) => {
+  return (
+    <div
+      className={clsx(styles.BackToTopBtn, {
+        [styles.OnScreenY]: isShowToTop,
+      })}
+    >
+      <Button circle blueGray ariaLabel="Go To Top" click={handleScrollToTop}>
+        <Fa white>fas fa-arrow-up</Fa>
+      </Button>
+    </div>
+  );
+};
 
 interface HomeLayoutProps extends RouterProps {
   path: string;
@@ -21,40 +98,35 @@ const HomeLayout: FC<HomeLayoutProps> = ({ children, location }) => {
     setIsShowToTop(false);
   };
 
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [percent, setPercent] = useState(0);
+  const handleDrawerOpen = () => {
+    setIsDrawerOpen(true);
+    setPercent(1);
+  };
+  const handleDrawerClose = () => {
+    setIsDrawerOpen(false);
+    setPercent(0);
+  };
+
   return (
     <>
-      <div
-        className={clsx(styles.NavBar, {
-          [styles.White]: isShowToTop,
-        })}
-      >
-        <Link
-          to="."
-          className={clsx(styles.NavBarLink, styles.NavBarHomeLink, {
-            [styles.Clickable]: isShowToTop || pathname !== '/',
-          })}
-        >
-          Calvin Hu
-        </Link>
-        <span className={styles.NavBarRight}>
-          <Link to="photo" className={styles.NavBarLink}>
-            Photography
-          </Link>
-          <Link to="about" className={styles.NavBarLink}>
-            About
-          </Link>
-        </span>
-      </div>
-      {children}
-      <div
-        className={clsx(styles.BackToTopBtn, {
-          [styles.OnScreenY]: isShowToTop,
-        })}
-      >
-        <Button circle blueGray ariaLabel="Go To Top" click={handleScrollToTop}>
-          <Fa white>fas fa-arrow-up</Fa>
-        </Button>
-      </div>
+      <NavBar
+        pathname={pathname}
+        handleDrawerOpen={handleDrawerOpen}
+        isShowToTop={isShowToTop}
+      />
+      <div className={styles.ContentContainer}>{children}</div>
+      <NavDrawer
+        handleDrawerClose={handleDrawerClose}
+        isDrawerOpen={isDrawerOpen}
+        navLinks={<Links onClick={handleDrawerClose} />}
+        percent={percent}
+      />
+      <TopOfPageButton
+        isShowToTop={isShowToTop}
+        handleScrollToTop={handleScrollToTop}
+      />
     </>
   );
 };
