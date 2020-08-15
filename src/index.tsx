c8413da065e281dfa93000db27b5d9ca00cd6803
Calvin Hu <ycalvinhu@gmail.com>
2020-08-14 21:16:25 -0700

    perf: remote unused packages

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import 'antd/dist/antd.css';
 import './index.module.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
